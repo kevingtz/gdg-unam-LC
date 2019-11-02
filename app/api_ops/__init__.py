d1f46779af875aa81f9d1357ef3625be5110a115
Kevin Loyola <me@kevinloy.app>
2019-11-02 11:17:11 -0600

    Final commit

--- app/api_ops/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-api_ops = Blueprint('api_ops', __name__)
-
-from . import ops, errors
--- app/auth/__init__.py
+++
@@ -1,7 +0,0 @@
-# HERE WE CREATE THE BLUEPRINT THAT WILL SUPPORT THE ROUTE OF OUR AUTHENTICATION SERVICE
-
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)  # REMEMBER ('NAME_OF_BLUEPRINT', MODULE WHERE II IS LOCATED)
-
-from . import views
